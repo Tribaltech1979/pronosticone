5d7cb280203cbb48542f8dbe8c0db48d0f15a9ce
Tribaltech1979 <tribaltech@gmail.com>
2015-08-02 14:53:18 +0200

    express-session

--- pronosticone/app.js
+++ pronosticone/app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session')
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -20,7 +21,10 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.session({secret : 'provaprovaprovaprova'}));
+app.use(session({secret : 'provaprovaprovaprova',
+                 resave: false,
+                 saveUninitialized: true
+                    }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 var mysql = require('mysql');
