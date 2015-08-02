9fc378a8a1ed8aad7657cb3aed044f6a076e65b8
Tribaltech1979 <tribaltech@gmail.com>
2015-08-02 14:25:37 +0200

    user login

--- pronosticone/routes/index.js
+++ pronosticone/routes/index.js
@@ -3,8 +3,95 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    if(req.session.utente){
+        res.redirect('/utente');
+    }
+    else {res.render('index', { title: 'Express' });}
 });
+//// LOGIN
+router.get('/login', function(req,res){
+    if(req.session.utente){
+        res.redirect('/utente');
+    }
+    else {res.render('login');}
+});
+//// UTENTE
+router.get('/utente',function(req,res){
+    if(req.session.utente){
+        var pool = req.pool;
+        var tmquery = "select * from v_squadre where cod_ute = "+req.session.utente;
+
+        pool.getConnection(function(err,connection){
+            if (err) {
+                connection.release();
+                res.json({"code" : 100, "status" : "Error in connection database"});
+                return;
+            }
+
+            console.log('connected as id ' + connection.threadId);
+
+            connection.query(tmquery,function(err,rows){
+                connection.release();
+                if(!err) {
+                    res.render('user', {"user" : rows});
+                }
+                else{
+                    res.redirect('/login');
+                }
+            });
+
+            connection.on('error', function(err) {
+                res.json({"code" : 100, "status" : "Error in connection database"});
+
+            });
+        });
+    }
+    else {res.redirect('/login');}
+});
+//////////////////////
+///// POST LOGIN
+///////////////////////
+router.post('/login',function(req,res){
+    var pool = req.pool;
+    var username = req.body.username;
+    var password = req.body.password;
+
+    var usrquery =  "select UTE_COD_UTENTE from Utenti where UTE_MAIL = '" + username + "' and UTE_PASS = '" + password+"'" ;
+
+    pool.getConnection(function(err,connection){
+        if (err) {
+            connection.release();
+            res.json({"code" : 100, "status" : "Error in connection database"});
+            return;
+        }
+
+        console.log('connected as id ' + connection.threadId);
+
+        connection.query(usrquery,function(err,rows){
+            connection.release();
+            if(!err) {
+                console.log(rows[0]);
+                req.session.utente = rows[0];
+                res.redirect('/');
+            }
+            else{
+                res.redirect('/login');
+            }
+        });
+
+        connection.on('error', function(err) {
+            res.json({"code" : 100, "status" : "Error in connection database"});
+
+        });
+    });
+}
+
+
+});
+
+///////////////////////
+/////USER LIST TEST
+/////////////////////
 
 router.get('/userlist',function(req,res){
         var pool = req.pool;
@@ -18,7 +105,7 @@ router.get('/userlist',function(req,res){
 
             console.log('connected as id ' + connection.threadId);
 
-            connection.query("select * from utenti",function(err,rows){
+            connection.query("select * from Utenti",function(err,rows){
                 connection.release();
                 if(!err) {
                     res.render('userlist',{
