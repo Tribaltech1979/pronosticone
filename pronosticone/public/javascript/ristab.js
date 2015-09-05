8270357d693e69fd855e470c581ea57ab2fe67ef
Tribaltech1979 <tribaltech@gmail.com>
2015-09-05 21:42:30 +0200

    colore

--- pronosticone/public/javascript/ristab.js
+++ pronosticone/public/javascript/ristab.js
@@ -6,6 +6,60 @@ $(document).ready(function () {
     $("td").css('width',$("img").width()/13+"px");
     $(".alert").hide();
 
+    if($(".G3").html()) {
+        colora();
+    }
 
-    $(".G9,.G10").each(function(){if($(this).val() && $(this).val()== 0){$(this).val('X');}})
+
+    $(".G5,.G14,.G15").each(function(){if($(this).val() && $(this).val()== 0){$(this).val('X');}})
 });
+
+function colora(){
+    ////segno
+    $(".G5").each(function () {
+
+        if( $(this).html() == $(this).parents("tr").find(".G14").html())  {
+            $(this).parents("tr").find(".G14").css('background-color','green');
+            if( $(this).parents("tr").find(".G16").html()){
+                $(this).parents("tr").find(".G16").css('background-color','green');
+            }
+        }
+
+        if( $(this).html() == $(this).parents("tr").find(".G15").html())  {
+            $(this).parents("tr").find(".G15").css('background-color','green');
+        }
+
+    });
+//// SOMMA GOL
+    $(".G11").each(function(){
+        if($(this).html() == ($(this).parents("tr").find(".G3").html()+$(this).parents("tr").find(".G4").html())){
+            $(this).css('background-color','green');
+            if( $(this).parents("tr").find(".G13").html()){
+                $(this).parents("tr").find(".G13").css('background-color','green');
+            }
+        }
+    });
+
+    $(".G12").each(function(){
+        if($(this).html() == ($(this).parents("tr").find(".G3").html()+$(this).parents("tr").find(".G4").html())){
+            $(this).css('background-color','green');
+        }
+    });
+
+/// RISULTATO ESATTO
+    $(".G6").each(function(){
+        if($(this).html()==$(this).parents("tr").find(".G3").html() && $(this).parents("tr").find(".G7").html()==$(this).parents("tr").find(".G4").html()){
+            $(this).css('background-color','green');
+            $(this).parents("tr").find(".G7").css('background-color','green');
+            if( $(this).parents("tr").find(".G10").html()){
+                $(this).parents("tr").find(".G10").css('background-color','green');
+            }
+        }
+    });
+    $(".G8").each(function(){
+        if($(this).html()==$(this).parents("tr").find(".G3").html() && $(this).parents("tr").find(".G9").html()==$(this).parents("tr").find(".G4").html()){
+            $(this).css('background-color','green');
+            $(this).parents("tr").find(".G9").css('background-color','green');
+        }
+    });
+}
