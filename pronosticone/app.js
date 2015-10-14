4efadcdc758d293149eb64c039bd262a8df5feae
Tribaltech1979 <tribaltech@gmail.com>
2015-10-14 21:03:17 +0200

    favico

--- pronosticone/app.js
+++ pronosticone/app.js
@@ -22,8 +22,7 @@ var configuration = JSON.parse(fs.readFileSync(dbfile));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicos.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'img','favicos.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
