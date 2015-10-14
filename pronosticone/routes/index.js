6480b626c63ac908bb30e1c71a41b5195fe1a2fa
Tribaltech1979 <tribaltech@gmail.com>
2015-10-14 22:39:31 +0200

    fix

--- pronosticone/routes/index.js
+++ pronosticone/routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+var fs = require('fs');
 var multer =   require('multer');
 var upload      =  multer({ dest: './public/img/team/',
 rename: function (fieldname, filename) {
