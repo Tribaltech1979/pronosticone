a3ed05fd67e040f2d28cefc8bcca414721911491
Tribaltech1979 <tribaltech@gmail.com>
2015-10-29 16:53:52 +0100

    commit 2

--- pronosticone/routes/users.js
+++ pronosticone/routes/users.js
@@ -2,5 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
+router.get('/signup', function(req, res){
+        res.render('signup');
+
+    });
 
 module.exports = router;
